570b31b95b8acd56d3990ffc57672e3569913947
darshan795 <darshanrout81@gmail.com>
2026-08-27 11:06:46 +0530

    praticed descending bubble sort

--- SORTING ALGORITHMS/BubbleSort.java
+++ SORTING ALGORITHMS/BubbleSort.java
@@ -18,6 +18,7 @@ class BubbleSort{
                         arr[j]=arr[j+1];
                         arr[j+1]=temp;
                     }
+                    printarr(arr);
 
 
             }
@@ -81,15 +82,29 @@ class BubbleSort{
             System.out.println(sorted);
             
     }
+     public static void descendingOrder(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]<arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+     }
 
     public static void  main(String[]args){
         int[] arr={5,1,2,3,4};
+        int [] arr2={1,6,2,5,4,3};
+
         System.out.println("darshan");
         String str="AZXYZXZAWERSDD";
         // System.out.println(str);
-        stringSorting(str);
+        descendingOrder(arr2);
+        printarr(arr2);
 
         
-
     }
 }
